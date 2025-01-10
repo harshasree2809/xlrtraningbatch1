3dc92ea75cb4a7bf4a663a952cfce2912c775c25
Harsha Sree <harshasree2809@gmail.com>
2025-01-10 13:20:47 +0530

    Add files via upload

---
+++ TEST001/Test002.java
@@ -0,0 +1,21 @@
+package PS003.TEST001;
+
+import java.util.Scanner;
+
+public class Test002 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String reply;
+        while(true){
+            System.out.print("Enter the value: ");
+            reply=sc.nextLine();
+            if(reply.equals("yes")){
+                System.out.println("Happy Sankrati");
+            }else{
+            System.out.println("Out of loop");
+            break;
+            }
+        }
+        sc.close();
+    }
+}
