3ee60d5ce2dbfe84f7f88e73325b112e99093277
Harsha Sree <harshasree2809@gmail.com>
2025-01-19 11:05:46 +0530

    Add files via upload

---
+++ core java/PS001/Emp.java
@@ -0,0 +1,34 @@
+package PS001;
+
+public class Emp {
+    static int Id;
+        String Firstname;
+        String Lastname;
+        String Address;
+        Emp(){
+           this.Id=0;
+           this.Firstname="";
+           this.Lastname="";
+           this.Address=""; 
+           
+        }
+        Emp(int Id,String Firstname,String Lastname,String Address){
+            this.Id=Id;
+            this.Firstname=Firstname;
+            this.Lastname=Lastname;
+            this.Address=Address;
+            System.out.println( Id+"\n"+Firstname+"\n"+Lastname+"\n"+Address);
+
+        }
+         void getFullname(){
+            
+            System.out.println(Firstname+" "+Lastname);
+        }
+        public static void main(String[] args){
+            Emp em =new Emp();
+            Emp emp=new Emp(461,"Luna","kai","kjhfertyu");
+            emp.getFullname();
+            
+    }
+
+}
