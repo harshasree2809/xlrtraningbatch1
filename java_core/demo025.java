c76dfdbbf977826f86e3c19eee248660c637b437
Harsha Sree <harshasree2809@gmail.com>
2025-01-23 11:38:44 +0530

    Array with name,address,pn

--- java_core/demo025.java
+++ java_core/demo025.java
@@ -1,8 +1,8 @@
 public class demo025 {
     public static void main(String[] args) {
-        String[] f1 = {"Mani ","Srujan ","Tarak ","venu ","vinay "};
-        String[] address= {"Hyd ", "KPHB ","KPHB ","Medak ","Medak "};
-        long[] number = {70324443,24890112,987839991,776891011,1090775432};
+        String[] f1 = {"Harshu ","Srujan ","Tarak ","priya ","teju ","nandini"};
+        String[] address= {"Hyd ", "KPHB ","KPHB ","Medak ","Medak "," hyd "};
+        long[] number = {70324443,24890112,987839991,776891011,1090775432,98765432};
 
         for(int i =0; i<f1.length; i++){
             System.out.println("Friend "+(i+1)+" :"+f1[i]+address[i]+number[i]);
