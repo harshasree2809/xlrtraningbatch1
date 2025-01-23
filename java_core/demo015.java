0356db42ef6bfbbf09bbe7c5b11a6c5b9514f144
Harsha Sree <harshasree2809@gmail.com>
2025-01-23 11:11:33 +0530

    Swapping of two elements

--- java_core/demo015.java
+++ java_core/demo015.java
@@ -1,17 +1,14 @@
 public class demo015 {
+   void swap(int a,int b){
+        System.out.println("Before swapping of two elements:\na="+a+" b="+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping of two elements:\na="+a+" b="+b);
+
+    }
     public static void main(String[] args) {
-        //swapping of elements
-        int x = 10;
-        int y =15;
-        System.out.println("x = "+x);
-        System.out.println("y = "+y);
-
-        int temp;
-        temp = x;
-        x = y;
-        y = temp;
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
-
+        Demo15 de=new Demo15();
+        de.swap(10,20);
     }
 }
