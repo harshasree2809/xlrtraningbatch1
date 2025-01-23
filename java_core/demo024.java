c51fda16045e3f3de2d1793c7a14a92b34d54432
Harsha Sree <harshasree2809@gmail.com>
2025-01-23 11:32:56 +0530

    string reverser

--- java_core/demo024.java
+++ java_core/demo024.java
@@ -7,5 +7,11 @@ class demo024{
         String rev = new StringBuilder(str).reverse().toString();
         System.out.println("entered string is:  "+str);
         System.out.println("Reversed String: "+rev);
+        // Scanner sc=new Scanner(System.in);
+        // String mess=sc.nextLine();
+        // StringBuilder remess = new StringBuilder(mess);
+        // remess.reverse();
+        // System.out.println(remess.toString());
+        // sc.close();
 }
 }
