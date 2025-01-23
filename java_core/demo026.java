df32b6e951247dc1dcaaf7a92cf4a2b15e9c17a6
Harsha Sree <harshasree2809@gmail.com>
2025-01-23 11:35:26 +0530

    Find vowel and consonant

--- java_core/demo026.java
+++ java_core/demo026.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 class demo026{
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+       //Find vowel and consonant
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the String: ");
-        String str = sc.nextLine();
-        //find only number of vowels and consonants in a string
-        int vowels = 0, consonants = 0;
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-                if (ch == 'a' || ch == 'e' || ch == 'i'|| ch == 'o' || ch == 'u') {
-                    vowels++;
-                    } else {
-                        consonants++;
-                        }
-                        }
-                        
-                        System.out.println("Number of vowels: " + vowels);
-                        System.out.println("Number of consonants: " + consonants);
-                    }                      
-
-                }
+        System.out.print("enter a char value: ");
+        char ch = sc.next().charAt(0);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+           ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            System.out.println("vowel");
+        } else {
+            System.out.println("consonant");
+        }
+        
+        
+        sc.close();
+    }
+}
