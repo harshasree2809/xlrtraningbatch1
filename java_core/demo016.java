669920a3cde4c3eeb133b97ddb23772f9da508e1
Harsha Sree <harshasree2809@gmail.com>
2025-01-23 11:13:34 +0530

    string reverser

--- java_core/demo016.java
+++ java_core/demo016.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 class demo016 {
     public static void main(String[] args) {
         //string reverser
+    //     public static void main(String[] args) {
+    //     Scanner sc=new Scanner(System.in);
+    //     String mess=sc.nextLine();
+    //     StringBuilder remess = new StringBuilder(mess);
+    //     remess.reverse();
+    //     System.out.println(remess.toString());
+    //     sc.close();
+
+    // }
         try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter the String: ");
             String str = sc.nextLine();
-
-                    
                     String reversed = new StringBuilder(str).reverse().toString();
-            
                     if (str.equals(reversed)) {
                         System.out.println("The string \"" + str + "\" is a palindrome.");
                     } else {
