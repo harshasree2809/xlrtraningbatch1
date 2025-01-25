5b659a1c88e8978c447a100e732c6d5552408950
Harsha Sree <harshasree2809@gmail.com>
2025-01-25 11:24:52 +0530

    sum of digits of a number

--- java_core/demo048.java
+++ java_core/demo048.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class demo048 {
-   
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
