114793f28d0e05c6f9f3d7bf6eba894d259d221a
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 17:14:49 +0530

    string reverser

--- java_core/Demo016.java
+++ java_core/Demo016.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class demo016 {
+class Demo016 {
     public static void main(String[] args) {
         //string reverser
     //     public static void main(String[] args) {
