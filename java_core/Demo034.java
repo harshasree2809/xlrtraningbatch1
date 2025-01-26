f03247c95a016f1169674a24b9456c530c23361e
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 18:04:16 +0530

    Find even or odd

--- java_core/Demo034.java
+++ java_core/Demo034.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo034 {
+public class Demo034 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
