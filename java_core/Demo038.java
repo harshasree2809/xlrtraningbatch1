6513ab29c3934e6bd7378ca82cfa744cc870c3a3
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 18:06:22 +0530

    Is uppercase or Is lowercase

--- java_core/Demo038.java
+++ java_core/Demo038.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo038 {
+public class Demo038 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a character: ");
