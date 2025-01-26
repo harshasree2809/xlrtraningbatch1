5be53d12ff0c6b060b8e31730df234e5ec33ab9f
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 18:04:40 +0530

    big number between 2 numbers

--- java_core/Demo035.java
+++ java_core/Demo035.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo035 {
+public class Demo035 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
