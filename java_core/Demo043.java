01ba096e9c10a2ab6484af79832890226cf8b474
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 18:08:42 +0530

    convert uppercase character into lower case

--- java_core/Demo043.java
+++ java_core/Demo043.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo043 {
+public class Demo043 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an uppercase character: ");
