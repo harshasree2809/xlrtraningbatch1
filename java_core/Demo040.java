a381b449491e476e27d9d74f21cf465a59f9505b
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 18:07:07 +0530

    Update and rename demo040.java to Demo040.java

--- java_core/Demo040.java
+++ java_core/Demo040.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo040 {
+public class Demo040 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a character: ");
