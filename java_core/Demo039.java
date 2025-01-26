4d48094e851dddfb26bb72f8513d373f2903ae69
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 18:06:46 +0530

    check character lowercase or uppercase or digit

--- java_core/Demo039.java
+++ java_core/Demo039.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo039 {
+public class Demo039 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a character: ");
