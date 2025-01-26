31144e8842afbe7f358dd7f59d40d024ae760edf
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 17:44:43 +0530

    Tax on Income

--- java_core/Demo022.java
+++ java_core/Demo022.java
@@ -22,7 +22,7 @@ class TaxOnIncome {
     }
 }
 
-public class demo022 {
+public class Demo022 {
     public static void main(String[] args) {
         //Tax on Income
         TaxOnIncome t = new TaxOnIncome();
