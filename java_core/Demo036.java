a3c23e1e70f1910aeb67cba7b93834dca776d4e7
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 18:05:29 +0530

    big number between 3 numbers

--- java_core/Demo036.java
+++ java_core/Demo036.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo036 {
+public class Demo036 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
