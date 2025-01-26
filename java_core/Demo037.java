a0f372e3388de4e97e57fb450af4294762d41ad5
Harsha Sree <harshasree2809@gmail.com>
2025-01-26 18:05:57 +0530

    big number between 4 numbers

--- java_core/Demo037.java
+++ java_core/Demo037.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo037 {
+public class Demo037 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
